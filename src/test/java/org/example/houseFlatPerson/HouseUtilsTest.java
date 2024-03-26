a1857461bc4913f685f430f5d25ec9ac8c865085
VladimirJun <111681640+VladimirJun@users.noreply.github.com>
2024-03-26 11:04:28 +0600

    lab7 second commit

---
+++ src/test/java/org/example/houseFlatPerson/HouseUtilsTest.java
@@ -0,0 +1,120 @@
+package org.example.houseFlatPerson;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HouseUtilsTest {
+    //task6
+    @Test
+    public void serialize() throws IOException {
+        House house = new House("12345", "123 Main St", new Person("Петров", "Владимир", "Юрьевич", "11.06.2004"), new ArrayList<>());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        HouseUtils.serialize(house, outputStream);
+
+        // Проверка, что outputStream содержит данные
+        assertTrue(outputStream.toByteArray().length > 0);
+    }
+
+    @Test
+    public void testDeserialize() throws IOException, ClassNotFoundException {
+
+        House expectedHouse = new House("12345", "123 Main St", new Person("Петров", "Владимир", "Юрьевич", "11.06.2004"), new ArrayList<>());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        HouseUtils.serialize(expectedHouse, outputStream);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        House deserializedHouse = HouseUtils.deserialize(inputStream);
+        // Проверка, что объекты house и deserializedHouse эквивалентны
+        assertEquals(expectedHouse.toString(), deserializedHouse.toString());
+    }
+
+    //task7
+    @Test
+    public void testWriteHouseToCsv() {
+        List<Person> owners1 = new ArrayList<>();
+        Person p1 = new Person("Иванов", "Иван", "Иванович", "01.01.01");
+        owners1.add(p1);
+        List<Person> owners2 = new ArrayList<>();
+        Person p2 = new Person("Сидоров", "Иван", "Иванович", "01.01.01");
+        owners2.add(p2);
+        Flat flat1 = new Flat(1, 100, owners1);
+        Flat flat2 = new Flat(2, 98, owners2);
+        List<Flat> flats = new ArrayList<>();
+        flats.add(flat1);
+        flats.add(flat2);
+        House house = new House("12345", "123 Main St", new Person("Петров", "Владимир", "Юрьевич", "11.06.2004"), flats);
+
+        try {
+            HouseUtils.writeHouseToCsv(house);
+        } finally {
+
+        }
+    }
+
+    @Test
+    public void testWriteHouseToCsv2() {
+        House house = new House("86422", "123 Main St", new Person("Петров", "Владимир", "Юрьевич", "11.06.2004"), new ArrayList<>());
+        HouseUtils.writeHouseToCsv(house);
+    }
+//task8
+@Test
+public void testSerializeJackson() throws IOException {
+
+    House house = new House("1", "House1", new Person("Петров", "Владимир", "Юрьевич", "11.06.2004"), new ArrayList<>());
+    String json = house.serializeJackson(house);
+    assertEquals(json,"{\"cadastralNumber\":\"1\",\"address\":\"House1\",\"houseHolder\":{\"surname\":\"Петров\",\"name\":\"Владимир\",\"patronymic\":\"Юрьевич\",\"birth\":\"11.06.2004\",\"fullName\":\"Петров Владимир Юрьевич\"},\"flats\":[]}");
+}
+
+    @Test
+    public void testDeserializeJackson() throws IOException {
+        House house = new House("1", "House1", new Person("Петров", "Владимир", "Юрьевич", "11.06.2004"), new ArrayList<>());
+
+        String houseString = "{\"cadastralNumber\":\"1\",\"address\":\"House1\",\"houseHolder\":{\"surname\":\"Петров\",\"name\":\"Владимир\",\"patronymic\":\"Юрьевич\",\"birth\":\"11.06.2004\",\"fullName\":\"Петров Владимир Юрьевич\"},\"flats\":[]}";
+        House deserializedHouse = house.deserializeJackson(houseString);
+    }
+    public void testDeserializeJackson2() throws IOException {
+        House house = new House("12345", "House2", new Person("Ильев", "Виктор", "Петрович", "22.08.1958"), new ArrayList<>());
+
+        String houseString = "{\"cadastralNumber\":\"12345\",\"address\":\"House2\",\"houseHolder\":{\"surname\":\"Ильев\",\"name\":\"Виктор\",\"patronymic\":\"Петрович\",\"birth\":\"22.08.1958\",\"fullName\":\"Ильев Виктор Петрович\"},\"flats\":[]}";
+        House deserializedHouse = house.deserializeJackson(houseString);
+    }
+//task9
+    @Test
+    public void testCompareJsonStrings_SameJson() throws IOException {
+        String json1 = "{\"id\": 1, \"name\": \"House 1\"}";
+        String json2 = "{\"id\": 1, \"name\": \"House 1\"}";
+
+        assertTrue(HouseUtils.compareJsonStrings(json1, json2));
+    }
+
+    @Test
+    public void testCompareJsonStrings_DifferentJson() throws IOException {
+        String json1 = "{\"id\": 1, \"name\": \"House 1\"}";
+        String json2 = "{\"id\": 2, \"name\": \"House 2\"}";
+
+        assertFalse(HouseUtils.compareJsonStrings(json1, json2));
+    }
+
+    @Test
+    public void testCompareJsonStrings_InvalidJson() throws IOException {
+        String json1 = "{\"id\": 1, \"name\": \"House 1\"}";
+        String json2 = "invalid json";
+
+        try {
+            HouseUtils.compareJsonStrings(json1, json2);
+            fail("Expected IOException was not thrown.");
+        } catch (IOException e) {
+            // Expected IOException
+        }
+    }
+
+
+}

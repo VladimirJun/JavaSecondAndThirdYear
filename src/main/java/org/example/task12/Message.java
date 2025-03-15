4b3140d816ee2dbf93069aebc28a20a831444616
VladimirJun <111681640+VladimirJun@users.noreply.github.com>
2025-03-15 15:11:57 +0600

    done with lab2

--- src/main/java/org/example/task12/Message.java
+++ src/main/java/org/example/task12/Message.java
@@ -1,5 +1,7 @@
 package org.example.task12;
 
+import java.util.Objects;
+
 public class Message {
     private final String emailAddress;
     private final String sender;
@@ -27,5 +29,17 @@ public class Message {
 
     public String getBody() {
         return body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(emailAddress, message.emailAddress) && Objects.equals(sender, message.sender) && Objects.equals(subject, message.subject) && Objects.equals(body, message.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(emailAddress, sender, subject, body);
     }
 }

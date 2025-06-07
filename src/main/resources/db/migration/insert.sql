00759986604cfe42becf3f7b1d3626ef205bdc7a
VladimirJun <Vovaboy3000@mail.ru>
2025-06-07 08:58:44 +0600

    edit migration

--- src/main/resources/db/migration/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO t_teachers (c_id, c_name, c_patronymic, c_surname)
-VALUES
-       (2, 'Artem', 'Dmitrievich', 'Malenov'),
-       (3, 'Andrey', 'Konstantinovich', 'Shelyagin')
---
+++ src/main/resources/db/migration/insert/V2__insert_teacher.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_teacher (c_id, c_name, c_patronymic, c_surname)
+VALUES (1, 'Oleg', 'Sergeevich', 'Nazarov'),
+       (2, 'Valeria', 'Evgenievna', 'Novikova'),
+       (3, 'Maksim', 'Anatolievich', 'Smirnov'),
+       (4, 'Kirill', 'Vladimirovich', 'Petrov');
